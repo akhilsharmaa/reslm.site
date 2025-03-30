900cfd86d9c19a2902d92ad5087e4ae7e8fce7aa
Akhilesh <akhilsharma3332@gmail.com>
2025-03-30 23:08:49 +0530

    [input validation] register user input validation completed.

--- server/src/routes/users/register.ts
+++ server/src/routes/users/register.ts
@@ -1,35 +1,52 @@
 import express, { Request, Response } from 'express';
-import { createHash } from "../../utils/password"
-import prisma from "../../database/prisma"
+import { createHash } from "../../utils/password";
+import prisma from "../../database/prisma";
+import { z } from "zod";
 
 const router = express.Router();
 
-router.post('/register', async (req: Request, res: Response): Promise<void> => {
-    
-    const { name, username, email, password } = req.body;      
-    let hashedPassword = await createHash(password); 
+const registerSchema = z.object({
+    name: z.string().min(2, "Name must be at least 2 characters long"),
+    username: z.string().min(3, "Username must be at least 3 characters long"),
+    email: z.string().email("Invalid email format"),
+    password: z.string().min(6, "Password must be at least 6 characters long"),
+});
 
-    try {  
+router.post('/register', async (req: Request, res: Response): Promise<void> => {
+    try { 
+
+        const { name, 
+                username, 
+                email, 
+                password } = registerSchema.parse(req.body);
+
+        const hashedPassword = await createHash(password);
+
         const newUser = await prisma.user.create({
             data: {
-                email: email, 
-                username: username, 
-                name: name, 
+                email,
+                username,
+                name,
                 password: hashedPassword
             }
-        }).then(() => {
-            res.status(200).json({ 
-                "message": "User registered successfully",  
-            }); 
-        })
+        });
+        
+        res.status(201).json({
+            message: "User registered successfully",
+        });
+        
+    } catch (error: any) {
 
-    } catch (error) {
-
-        console.error('Error creating user:', error); 
-
-        res.status(400).json({ 
-            "message": "Failed to registered user, please try again later.",  
-        }); 
+        if (error instanceof z.ZodError) {
+            res.status(400).json({ message: "Validation error", errors: error.errors });
+        }
+        
+        if (error.code === "P2002") {
+            res.status(409).json({ message: "Username or email already exists" });
+        }
+        
+        console.error('Error creating user:', error);
+        res.status(500).json({ message: "Failed to register user, please try again later." });
     }
 });
 
